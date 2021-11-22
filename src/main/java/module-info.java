b9eac931460383c9199655baf650bf0f6bc9fd90
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-11-22 15:53:16 -0800

    Added requires keyword for gson so the project recognize the gson library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.f21comp1011s1assignment2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
     opens com.example.f21comp1011s1assignment2 to javafx.fxml;
