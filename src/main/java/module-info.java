368f993a5cde337b96a5d9631ac3fb8cb8100d55
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-11-23 13:10:05 -0800

    Added modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.example.f21comp1011s1assignment2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires com.google.gson;
+    requires java.net.http; //module for HttpClient class
 
-
-    opens com.example.f21comp1011s1assignment2 to javafx.fxml;
+    //Allow you to build the project exporting gson through the project
+    opens com.example.f21comp1011s1assignment2 to javafx.fxml, com.google.gson;
     exports com.example.f21comp1011s1assignment2;
 }
